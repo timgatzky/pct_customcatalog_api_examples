927f32a40343664deac9aa92686641cbb961d94d
Tim Gatzky <info@tim-gatzky.de>
2016-09-21 09:30:50 +0200

    update

--- system/modules/pct_customcatalog_api_examples/config/config.php
+++ system/modules/pct_customcatalog_api_examples/config/config.php
@@ -15,13 +15,4 @@ $GLOBALS['PCT_CUSTOMCATALOG']['API']['myApi'] = array
 (
 	'label'		=> &$GLOBALS['TL_LANG']['PCT_CUSTOMCATALOG']['API']['myApi'],
 	'class'		=> 'MyCustomCatalogApi', // The default class if no callbacks for modes have been registered
-	'modes' 	=> array
-	(
-		// register the "myMode" mode
-		'myMode' => array
-		(
-			// register its own callback method
-			'callback' 	=> array('MyCustomCatalogApi','run'),
-		)
-	)
 );
